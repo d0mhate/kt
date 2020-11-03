f672a7be68ec7535ba9f3f3c0b5b038e6c7d09b6
Anatoly <41338792+d0mhate@users.noreply.github.com>
2020-11-03 11:21:14 +0400

    commit;

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,83 +3,174 @@
 namespace App\Http\Controllers;
 
 use App\Task;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
+    const STATUS = ['finished', 'progress', 'terminated'];
+
+    /* @var \Illuminate\Http\Request $request */
+    protected $request;
+    /* @var \Illuminate\Routing\ResponseFactory $response */
+    protected $response;
+
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * MobileApiController constructor.
      */
-    public function index()
+    public function __construct()
     {
-        //
+        $this->response = response();
+        $this->middleware(function ($request, $next) {
+            $this->request = $request;
+            return $next($request);
+        });
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Task[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Http\Response
      */
-    public function create()
+    public function index()
     {
-        //
+        return Task::all();
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @return void
+     * @throws Exception
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        $validate = Validator::make(request()->all(), [
+            'title'  => 'required',
+            'body'   => 'required',
+            'status' => 'required|in:' . implode(',', self::STATUS),
+        ]);
+        $this->checkValidationFails($validate);
+        $task = Task::create(request()->all());
+        $task->save();
+        $this->sendResponse([
+            'status'  => 'success',
+            'message' => 'Задача создана',
+            'task_id' => $task->id
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
+     * @return void
+     * @throws Exception
      */
-    public function show(Task $task)
+    public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Task $task)
-    {
-        //
+        $issueTask = Task::find($id);
+        if (!$issueTask) {
+            $this->sendResponse(
+                [
+                    'status'  => 'error',
+                    'message' => 'Задача не найдена',
+                ],
+                404
+            );
+        } else {
+            return $issueTask;
+        }
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
+     * @param $id
+     * @return void
+     * @throws Exception
      */
-    public function update(Request $request, Task $task)
+    public function update($id)
     {
-        //
+        $verifiedArray = array_merge(['id' => $id], $this->request->all());
+        $valid = Validator::make($verifiedArray, [
+            'id'     => 'required|exists:tasks,id',
+            'title'  => 'required',
+            'body'   => 'required',
+            'status' => 'required|in:' . implode(',', self::STATUS),
+        ], [
+            'id.required' => 'ID обязательно',
+            'id.exists'   => "Задача с ID: $id не существует",
+        ]);
+        $this->checkValidationFails($valid);
+        $updatedModel = Task::find($id);
+        if ($updatedModel) {
+            $updatedModel->title = $this->request->title;
+            $updatedModel->body = $this->request->body;
+            $updatedModel->user_id = $this->request->user_id ?? null;
+            $updatedModel->save();
+            $this->sendResponse(
+                [
+                    'status'  => 'success',
+                    'message' => 'Задача обновлена',
+                    'task_id' => $updatedModel->id
+                ]
+            );
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
+     * @param $id
+     * @return void
+     * @throws Exception
      */
-    public function destroy(Task $task)
+    public function destroy($id)
     {
-        //
+        $valid = Validator::make(['id' => $id], [
+            'id' => 'required|exists:tasks,id',
+        ], [
+            'id.required' => 'ID обязательно',
+            'id.exists'   => "Задача с ID: $id не существует",
+        ]);
+        $this->checkValidationFails($valid);
+        $post = Task::find($id);
+        $post->delete();
+        //softDelete ?
+        $this->sendResponse([
+            'status'  => 'success',
+            'message' => 'Задача удалена',
+            'task_id' => $post->id
+        ]);
+    }
+
+    /**
+     * Проверка валидации
+     * При неудавшейся валидации выбрасывает исключение
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @throws Exception
+     */
+    protected function checkValidationFails(\Illuminate\Contracts\Validation\Validator $validator): void
+    {
+        if ($validator->fails()) {
+            $errors = implode("\n ", $validator->errors()->all());
+            $this->sendResponse($validator->errors()->all(), 404);
+            throw new Exception($errors, 400);
+        }
+    }
+
+    /**
+     * Отпавка ответа
+     * @param array | Collection $data
+     * @param int $code
+     * @return null
+     */
+    protected function sendResponse($data, int $code = 200)
+    {
+        $this->response->json($data, $code, ['Content-Type' => 'application/json'])->send();
+        return null;
     }
 }
